ee53928a27a26ee31345045d9d5b26a8024cd2fe
Mislav Marohnić <mislav.marohnic@gmail.com>
2009-11-01 17:13:39 +0100

    login with Twitter

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'yaml'
 require 'sinatra'
+require 'twitter'
 
 require 'dm-core'
 require 'dm-timestamps'
@@ -10,12 +11,22 @@ require 'sass'
 require 'nokogiri'
 
 set :haml, { :format => :html5 }
+enable :sessions
+
+module Egotrip
+  ConfigFile = ENV['CONFIG'] || File.dirname(__FILE__) + '/config.yml'
+  
+  def self.config
+    @config ||= YAML::load File.read(ConfigFile)
+  end
+  
+  def self.oauth(options = {})
+    Twitter::OAuth.new(config[:oauth][:key], config[:oauth][:secret], options)
+  end
+end
 
 configure do
-  config_file = ENV['CONFIG'] || File.dirname(__FILE__) + '/config.yml'
-  config = YAML::load File.read(config_file)
-  
-  dbconfig = config[:database]
+  dbconfig = Egotrip.config[:database]
   adapter = DataMapper.setup(:default, dbconfig)
   
   if ':memory:' == adapter.options['path']
@@ -24,6 +35,17 @@ configure do
 end
 
 helpers do
+  def url_for(path)
+    url = request.scheme + "://"+ request.host
+
+    if request.scheme == "https" && request.port != 443 ||
+        request.scheme == "http" && request.port != 80
+      url << ":#{request.port}"
+    end
+
+    url << path
+  end
+  
   def twitter_url(user)
     "http://twitter.com/#{user.screen_name}"
   end
@@ -31,11 +53,49 @@ helpers do
   def link_to(text, href)
     haml_tag :a, text, :href => href
   end
+  
+  def logged_in?
+    session[:user_id]
+  end
+  
+  def current_user
+    @current_user ||= User.get(session[:user_id])
+  end
 end
 
 get '/' do
   @users = User.all
   haml :index
+end
+
+get '/login' do
+  oauth = Egotrip.oauth(:sign_in => true)
+  oauth.set_callback_url url_for('/authorized')
+  token = oauth.request_token
+  session[:rtoken], session[:rsecret] = token.token, token.secret
+  redirect token.authorize_url
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/'
+end
+
+get '/authorized' do
+  rtoken, rsecret = session[:rtoken], session[:rsecret]
+  oauth = Egotrip.oauth
+  oauth.authorize_from_request rtoken, rsecret, params[:oauth_verifier]
+  twitter = Twitter::Base.new(oauth)
+  cred = twitter.verify_credentials
+  
+  user = User.first_or_create({:screen_name => cred.screen_name}, {
+    :full_name => cred.name, :avatar_url => cred.profile_image_url,
+    :followers_count => cred.followers_count, :following_count => cred.friends_count,
+    :tweets_count => cred.statuses_count
+  })
+  session[:user_id] = user.id
+  
+  redirect '/'
 end
 
 post '/' do
